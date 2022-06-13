362f131f19987ff200dd0358507ee07831593f0f
pitlover <pitlover@naver.com>
2022-06-13 21:58:22 +0900

    [ADD] 짝지어 제거하기

---
+++ Lv2/12973.py
@@ -0,0 +1,26 @@
+# 짝지어 제거하기
+# Success
+
+from collections import deque
+
+def solution(s):
+    s = list(s)
+    queue = deque()
+    queue.append(s[0])
+
+    while queue:
+        for a in range(1, len(s)):
+            if len(queue) == 0:
+                queue.append(s[a])
+                continue
+
+            if queue[-1] == s[a]:
+                queue.pop()
+            else:
+                queue.append(s[a])
+        break
+
+    if len(queue) == 0:
+        return 1
+    else:
+        return 0
