4db12bf64ccfe8a43bc84d25ce1c63e1b19cc9c1
pitlover <49435880+pitlover@users.noreply.github.com>
2022-06-21 01:02:23 +0900

    [ADD] 12911.py

--- Lv2/12911.py
+++ Lv2/12911.py
@@ -1,9 +1,8 @@
+# 다음 큰 숫자
+# Success
+
 def solution(n):
     count_one = bin(n).count("1")
     for a in range(n+1, 1<<20):
-        if bin(a).count("1") != count_one:
-            continue
-        elif a > n:
+        if bin(a).count("1") == count_one:
             return a
-                
-            
