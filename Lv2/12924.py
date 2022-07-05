e250dfe7bf3dc0748600bf3fd65ff44e6ba2797a
pitlover <49435880+pitlover@users.noreply.github.com>
2022-07-06 00:05:00 +0900

    [FIX] 숫자의 표현

--- Lv2/12924.py
+++ Lv2/12924.py
@@ -16,3 +16,25 @@ def solution(n):
         if tmp[-2] > n:
             break
     return dp[-1] + 1
+
+## another sol
+
+def solution(n):
+    if n == 1 and n == 2:
+        return 1
+    elif n == 3:
+        return 2
+    
+    dp = [1 for _ in range(n+1)]
+    tmp = [3, 5, 6]
+    dp[1], dp[2], dp[3] = 1, 1, 2
+    for a in range(4, (n+1)//2+1):
+        tmp.insert(0, 0)
+        for index, b in enumerate(tmp):
+            tmp[index] += a
+            if tmp[index] <= n:
+                dp[tmp[index]] += 1
+            else:
+                break
+                
+    return dp[n]
