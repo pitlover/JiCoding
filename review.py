64863f0592b12024072a277289dbe3633c7e42e1
pitlover <49435880+pitlover@users.noreply.github.com>
2022-07-25 23:15:35 +0900

    [FIX] review.py

--- review.py
+++ review.py
@@ -118,3 +118,14 @@ q.pop()     # 6
 q.popleft() # 0
 #seek
 q[0]
+
+
+# 소수 구하기
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
+
