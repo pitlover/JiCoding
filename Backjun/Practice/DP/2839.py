c6c65c7dd2c1ba103a94cf14ef9aca422edeecd0
pitlover <pitlover@naver.com>
2022-12-06 16:32:48 +0900

    [ADD] 설탕배달

---
+++ Backjun/Practice/DP/2839.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+dp = [-1] * (5001)
+dp[3] = 1
+dp[5] = 1
+
+for num in range(6, n+1):
+    if dp[num-3] != -1 and dp[num-5] != -1:
+        dp[num] = min(dp[num-3], dp[num-5]) + 1
+    elif dp[num-3] == -1 and dp[num-5] != -1:
+        dp[num] = dp[num-5] + 1
+    elif dp[num-3] != -1 and dp[num-5] == -1:
+        dp[num] = dp[num-3] + 1
+
+print(dp[n])
+
