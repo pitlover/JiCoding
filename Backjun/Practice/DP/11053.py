86f6eaca777688e992c81df627560d52f2454a14
pitlover <pitlover@naver.com>
2022-12-13 16:25:01 +0900

    [ADD] 가장 긴 부분 수열

--- Backjun/Practice/DP/11053.py
+++ Backjun/Practice/DP/11053.py
@@ -9,5 +9,5 @@ dp = [1] * (n)
 for i in range(n):
     for j in range(i):
         if num[i] > num[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+            dp[i] = max(dp[i], dp[j] + 1) 
 print(max(dp))
