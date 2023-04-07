1605127e4ef414756f739bee88a33930746fb0ec
jiyoungkim <pitlover@naver.com>
2023-04-07 18:25:15 +0900

    [ADD] 소수만들

--- Programmers/Lv1/12977.py
+++ Programmers/Lv1/12977.py
@@ -1,6 +1,8 @@
 # 소수 만들기
 # Success
 
+'''
+# Old
 def is_prime(num):
     import math
     for a in range(2, int(math.sqrt(num))+1):
@@ -18,3 +20,27 @@ def solution(nums):
         if is_prime(a) == 1:
             answer += 1
     return answer
+'''
+import math
+from itertools import combinations
+
+
+def is_prime(num):
+    for a in range(2, int(math.sqrt(num)) + 1):
+        if num % a == 0:
+            return False
+    return True
+
+
+def solution(nums):
+    combs = combinations(nums, 3)
+    result = 0
+    for comb in combs:
+        if is_prime(sum(comb)):
+            result += 1
+
+    return result
+
+
+solution([1, 2, 3, 4])
+solution([1, 2, 7, 6, 4])
