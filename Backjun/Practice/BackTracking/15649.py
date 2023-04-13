0cf131b472b1dc5bfee7847a8228eaf919f79ad0
jiyoungkim <pitlover@naver.com>
2023-04-13 20:27:52 +0900

    [ADD] N과 M (1)

--- Backjun/Practice/BackTracking/15649.py
+++ Backjun/Practice/BackTracking/15649.py
@@ -1,7 +1,38 @@
+# ----- Sol 1 ------ #
+'''
 n, m = map(int, input().split())
 from itertools import permutations
 
-results = list(permutations(range(1, n+1), m))
+results = list(permutations(range(1, n + 1), m))
 
 for a in results:
     print(*a)
+
+'''
+
+# Backtracking with DFS
+
+n, m = map(int, input().split())
+
+s = []
+visited = [False] * (n + 1)
+
+
+def dfs():
+    global s
+
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs()
+        s.pop()
+        visited[i] = False
+
+
+dfs()
