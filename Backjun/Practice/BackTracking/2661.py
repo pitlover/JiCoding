dfe15af45891cbc00441bcc95da41efc9655fa89
jiyoungkim <pitlover@naver.com>
2023-04-14 13:24:17 +0900

    [ADD] 좋은 수열

--- Backjun/Practice/BackTracking/2661.py
+++ Backjun/Practice/BackTracking/2661.py
@@ -3,7 +3,29 @@
 '''
 
 n = int(input())
+
+
+def check(arr):
+    for i in range(1, len(arr) // 2 + 1):
+        if arr[-i:] == arr[-2 * i: -i]:
+            return False
+    return True
+
+
+def dfs(arr):
+    if len(arr) == n:
+        print("".join(map(str, arr)))
+        exit()
+
+    for i in range(1, 4):
+        arr.append(i)
+        if check(arr):
+            dfs(arr)
+        arr.pop()
+
+
 if n == 1:
     print(1)
     exit()
 
+dfs([])
