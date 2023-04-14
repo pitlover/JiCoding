c4774d7d806d1d39eb935d56a37e059723d29ecf
jiyoungkim <pitlover@naver.com>
2023-04-14 12:23:55 +0900

    [ADD] N-Queen

--- Backjun/Practice/BackTracking/9663.py
+++ Backjun/Practice/BackTracking/9663.py
@@ -38,3 +38,47 @@ if n == 1:
     exit()
 dfs([])
 print(result)
+
+'''
+'''
+8
+'''
+# (0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596)
+result = 0
+
+
+def is_update(arr, i):
+    # i : (len(arr), i), arr : (idx, loc)
+    for idx, loc in enumerate(arr):
+        if loc == i or (abs(len(arr) - idx) == abs(i - loc)):
+            return False
+    return True
+
+
+def dfs(arr):
+    if len(arr) == n:
+        global result
+        result += 1
+        return
+
+    for i in range(n):
+        if is_visited[i]:
+            continue
+
+        if is_update(arr, i):
+            arr.append(i)
+            is_visited[i] = True
+            dfs(arr)
+            arr.pop()
+            is_visited[i] = False
+
+
+n = int(input())
+if n == 1:
+    print(1)
+    exit()
+is_visited = [False] * (n)
+dfs([])
+print(result)
+
+'''
